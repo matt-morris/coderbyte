3100277fc579dca3e57f7c5f391f766a9f5ba80b
Matt Morris <matt.morris@placewise.com>
2015-01-06 18:49:06 -0700

    add solution to first problem

---
+++ 1.js
@@ -0,0 +1,12 @@
+/*
+ *  First Reverse
+ *  For this challenge you will be reversing a string.
+ */
+
+function FirstReverse(str) {
+  return str.split('').reverse().join('');
+}
+   
+// keep this function call here 
+// to see how to enter arguments in JavaScript scroll down
+FirstReverse(readline());
