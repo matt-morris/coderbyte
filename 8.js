29c2d9da45150a8550e353a9d34a944c6fe35fb5
Matt Morris <matt.morris@placewise.com>
2015-01-19 21:45:22 -0700

    initial solution for problem eight

---
+++ 8.js
@@ -0,0 +1,11 @@
+/*
+ *  Check Nums
+ *  For this challenge you will be comparing two numbers and determining which one is greater.
+ *
+ *
+ */
+ function CheckNums(num1,num2) { 
+   if (num1 < num2) return 'true';
+   if (num1 > num2) return 'false';
+   return -1;
+ }
