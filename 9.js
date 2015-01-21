7192fc0233902dc430548cc2124496d2ef975e14
Matt Morris <matt.morris@placewise.com>
2015-01-20 20:38:15 -0700

    add initial solution for nine

---
+++ 9.js
@@ -0,0 +1,9 @@
+/*
+ *  Time Convert
+ *  For this challenge you will be determining the difference in hours and minutes between two given times.
+ *
+ *  Using the JavaScript language, have the function TimeConvert(num) take the num parameter being passed and return the number of hours and minutes the parameter converts to (ie. if num = 63 then the output should be 1:3). Separate the number of hours and minutes with a colon.
+ */
+function TimeConvert(num) {
+  return parseInt((num / 60), 10) + ':' + (num % 60);
+}
