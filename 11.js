718218ecb42914f7f18d943fba7d5d9b3a6c3279
Matt Morris <matt.morris@placewise.com>
2015-01-20 21:04:06 -0700

    shorten ABCheck to one line

--- 11.js
+++ 11.js
@@ -5,10 +5,6 @@
  *  Using the JavaScript language, have the function ABCheck(str) take the str parameter being passed and return the string true if the characters a and b are separated by exactly 3 places anywhere in the string at least once (ie. "lane borrowed" would result in true because there is exactly three characters between a and b). Otherwise return the string false.
  *
  */
- function ABCheck(str) {
-   if (str.match(/a.{3}b/)) {
-     return true;
-   } else {
-     return false;
-   }
- }
+function ABCheck(str) {
+  return str.match(/a.{3}b/) !== null;
+}
