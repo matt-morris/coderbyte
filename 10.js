47e9fd9daceb995b533ae541ffc29c44505bb436
Matt Morris <matt.morris@placewise.com>
2015-01-20 20:44:10 -0700

    add initial solution for problem ten

---
+++ 10.js
@@ -0,0 +1,9 @@
+/*
+ *  Alphabet Soup
+ *  For this challenge you will be sorting characters in a string.
+ *
+ *  Using the JavaScript language, have the function AlphabetSoup(str) take the str string parameter being passed and return the string with the letters in alphabetical order (ie. hello becomes ehllo). Assume numbers and punctuation symbols will not be included in the string.
+ */
+function AlphabetSoup(str) {
+  return str.split('').sort().join('');
+}
