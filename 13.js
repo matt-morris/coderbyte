0bb8a253ce5a39cea0e0c594da14a0ccc8628be5
Matt Morris <matt.morris@placewise.com>
2015-01-21 21:12:47 -0700

    add initial solution for problem thirteen

---
+++ 13.js
@@ -0,0 +1,9 @@
+/*
+ *  Word Count
+ *  For this challenge you will be determining how many words a sentence contains.
+ *
+ *  Using the JavaScript language, have the function WordCount(str) take the str string parameter being passed and return the number of words the string contains (ie. "Never eat shredded wheat" would return 4). Words will be separated by single spaces.
+ */
+function WordCount(str) {
+  return str.split(/\w/).length;
+}
