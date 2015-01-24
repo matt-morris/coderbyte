335be5f73e8c9d1e6194b9a0ce2eb96e417c6b44
Matt Morris <matt.morris@placewise.com>
2015-01-24 08:15:11 -0700

    add initial solution for thirty-four

---
+++ 34.js
@@ -0,0 +1,9 @@
+/*
+ *  Palindrome Two
+ *  For this challenge you will be determining if a string is a palindrome.
+ *
+ *  Using the JavaScript language, have the function PalindromeTwo(str) take the str parameter being passed and return the string true if the parameter is a palindrome, (the string is the same forward as it is backward) otherwise return the string false. The parameter entered may have punctuation and symbols but they should not affect whether the string is in fact a palindrome. For example: "Anne, I vote more cars race Rome-to-Vienna" should return true.
+ */
+function PalindromeTwo(str) {
+  return str.toLowerCase().match(/[a-z]/g).join('') === str.toLowerCase().match(/[a-z]/g).reverse().join('');
+}
